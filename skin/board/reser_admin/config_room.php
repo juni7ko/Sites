bf97614aae59c188372091994c41304c0b5f5605
Juni7 <linux7@naver.com>
2013-12-31 15:13:05 +0900

    객실이미지 작업.

--- skin/board/reser_admin/config_room.php
+++ skin/board/reser_admin/config_room.php
@@ -13,6 +13,8 @@ if ($board[bo_content_head]) echo stripslashes($board[bo_content_head]);
 ############# 헤드
 $this_page = "{$_SERVER['PHP_SELF']}?bo_table={$bo_table}";
 
+$upload_max_filesize = ini_get('upload_max_filesize');
+
 $file = get_file_room("bbs34_r_info", $id);
 
 // 가변 파일
@@ -381,37 +383,199 @@ if($u == "add") {
 			WHERE r_info_id ='$id' LIMIT 1 ;";
 
 		$result = sql_query($sql);
-		Up_Cate($bo_table);
 
 // File Upload <-
-		if ($w == "u")
-		{
-		    for ($i=0; $i<$file[count]; $i++)
-		    {
-				$fsql = " select bf_file, bf_content from $g4[board_file_table] where bo_table = '$bo_table' and wr_id = '$pension_id' and bf_no = '$i' ";
-			//	echo $fsql;
-		        $row = sql_fetch($fsql);
-		        if ($row[bf_file])
-		        {
-		            $file_script .= "add_file(\" <input type='checkbox' name='bf_file_del[$i]' value='1'>  <img src=../data/file/".$bo_table."/".$row[bf_file]." width=80> <a href='{$file[$i][href]}'>{$file[$i][source]}({$file[$i][size]})</a>  파일 삭제 ";
+		// 디렉토리가 없다면 생성합니다. (퍼미션도 변경하구요.)
+		@mkdir("$g4[path]/data/file/roomFile", 0707);
+		@chmod("$g4[path]/data/file/roomFile", 0707);
 
-		            if ($is_file_content)
-		                //$file_script .= "<br><input type='text' class=ed size=50 name='bf_content[$i]' value='{$row[bf_content]}' title='업로드 이미지 파일에 해당 되는 내용을 입력하세요.'>";
-		                // 첨부파일설명에서 ' 또는 " 입력되면 오류나는 부분 수정
-		                $file_script .= "<br><input type='text' class=ed size=50 name='bf_content[$i]' value='".addslashes(get_text($row[bf_content]))."' title='업로드 이미지 파일에 해당 되는 내용을 입력하세요.'>";
-		            $file_script .= "\");\n";
+		$chars_array = array_merge(range(0,9), range('a','z'), range('A','Z'));
+		//print_r2($chars_array); exit;
+
+		// 가변 파일 업로드
+		$file_upload_msg = "";
+		$upload = array();
+
+		for ($i=0; $i<count($_FILES[bf_file][name]); $i++)
+		{
+		    // 삭제에 체크가 되어있다면 파일을 삭제합니다.
+		    if ($_POST[bf_file_del][$i])
+		    {
+		        $upload[$i][del_check] = true;
+
+		        $row = sql_fetch(" select bf_file from $g4[pension_file_table] where bo_table = '$bo_table' and wr_id = '$id' and bf_no = '$i' ");
+		        @unlink("$g4[path]/data/file/roomFile/$row[bf_file]");
+		    }
+		    else
+		        $upload[$i][del_check] = false;
+
+		    $tmp_file  = $_FILES[bf_file][tmp_name][$i];
+		    $filesize  = $_FILES[bf_file][size][$i];
+		    $filename  = $_FILES[bf_file][name][$i];
+		    $filename  = preg_replace('/(\s|\<|\>|\=|\(|\))/', '_', $filename);
+
+		    // 서버에 설정된 값보다 큰파일을 업로드 한다면
+		    if ($filename)
+		    {
+		        if ($_FILES[bf_file][error][$i] == 1)
+		        {
+		            $file_upload_msg .= "\'{$filename}\' 파일의 용량이 서버에 설정($upload_max_filesize)된 값보다 크므로 업로드 할 수 없습니다.\\n";
+		            continue;
+		        }
+		        else if ($_FILES[bf_file][error][$i] != 0)
+		        {
+		            $file_upload_msg .= "\'{$filename}\' 파일이 정상적으로 업로드 되지 않았습니다.\\n";
+		            continue;
+		        }
+		    }
+
+		    if (is_uploaded_file($tmp_file))
+		    {
+		        // 관리자가 아니면서 설정한 업로드 사이즈보다 크다면 건너뜀
+		        if (!$is_admin && $filesize > $board[bo_upload_size])
+		        {
+		            $file_upload_msg .= "\'{$filename}\' 파일의 용량(".number_format($filesize)." 바이트)이 게시판에 설정(".number_format($board[bo_upload_size])." 바이트)된 값보다 크므로 업로드 하지 않습니다.\\n";
+		            continue;
+		        }
+
+		        //=================================================================\
+		        // 090714
+		        // 이미지나 플래시 파일에 악성코드를 심어 업로드 하는 경우를 방지
+		        // 에러메세지는 출력하지 않는다.
+		        //-----------------------------------------------------------------
+		        $timg = @getimagesize($tmp_file);
+		        // image type
+		        if ( preg_match("/\.($config[cf_image_extension])$/i", $filename) ||
+		             preg_match("/\.($config[cf_flash_extension])$/i", $filename) )
+		        {
+		            if ($timg[2] < 1 || $timg[2] > 16)
+		            {
+		                //$file_upload_msg .= "\'{$filename}\' 파일이 이미지나 플래시 파일이 아닙니다.\\n";
+		                continue;
+		            }
+		        }
+		        //=================================================================
+
+		        $upload[$i][image] = $timg;
+
+		        // 4.00.11 - 글답변에서 파일 업로드시 원글의 파일이 삭제되는 오류를 수정
+		        if ($w == 'u')
+		        {
+		            // 존재하는 파일이 있다면 삭제합니다.
+		            $row = sql_fetch(" select bf_file from $g4[pension_file_table] where bo_table = '$bo_table' and wr_id = '$id' and bf_no = '$i' ");
+		            @unlink("$g4[path]/data/file/roomFile/$row[bf_file]");
+		        }
+
+		        // 프로그램 원래 파일명
+		        $upload[$i][source] = $filename;
+		        $upload[$i][filesize] = $filesize;
+
+		        // 아래의 문자열이 들어간 파일은 -x 를 붙여서 웹경로를 알더라도 실행을 하지 못하도록 함
+		        $filename = preg_replace("/\.(php|phtm|htm|cgi|pl|exe|jsp|asp|inc)/i", "$0-x", $filename);
+
+		        // 접미사를 붙인 파일명
+		        //$upload[$i][file] = abs(ip2long($_SERVER[REMOTE_ADDR])).'_'.substr(md5(uniqid($g4[server_time])),0,8).'_'.urlencode($filename);
+		        // 달빛온도님 수정 : 한글파일은 urlencode($filename) 처리를 할경우 '%'를 붙여주게 되는데 '%'표시는 미디어플레이어가 인식을 못하기 때문에 재생이 안됩니다. 그래서 변경한 파일명에서 '%'부분을 빼주면 해결됩니다.
+		        //$upload[$i][file] = abs(ip2long($_SERVER[REMOTE_ADDR])).'_'.substr(md5(uniqid($g4[server_time])),0,8).'_'.str_replace('%', '', urlencode($filename));
+		        shuffle($chars_array);
+		        $shuffle = implode("", $chars_array);
+
+		        // 첨부파일 첨부시 첨부파일명에 공백이 포함되어 있으면 일부 PC에서 보이지 않거나 다운로드 되지 않는 현상이 있습니다. (길상여의 님 090925)
+		        //$upload[$i][file] = abs(ip2long($_SERVER[REMOTE_ADDR])).'_'.substr($shuffle,0,8).'_'.str_replace('%', '', urlencode($filename));
+		        $upload[$i][file] = abs(ip2long($_SERVER[REMOTE_ADDR])).'_'.substr($shuffle,0,8).'_'.str_replace('%', '', urlencode(str_replace(' ', '_', $filename)));
+
+		        $dest_file = "$g4[path]/data/file/roomFile/" . $upload[$i][file];
+
+		        // 업로드가 안된다면 에러메세지 출력하고 죽어버립니다.
+		        $error_code = move_uploaded_file($tmp_file, $dest_file) or die($_FILES[bf_file][error][$i]);
+
+		        // 올라간 파일의 퍼미션을 변경합니다.
+		        chmod($dest_file, 0606);
+
+		        //$upload[$i][image] = @getimagesize($dest_file);
+
+		    }
+		}
+
+
+		//------------------------------------------------------------------------------
+		// 가변 파일 업로드
+		// 나중에 테이블에 저장하는 이유는 $wr_id 값을 저장해야 하기 때문입니다.
+		for ($i=0; $i<count($upload); $i++)
+		{
+		    if (!get_magic_quotes_gpc()) {
+		        $upload[$i]['source'] = addslashes($upload[$i]['source']);
+		    }
+
+		    $row = sql_fetch(" select count(*) as cnt from $g4[pension_file_table] where bo_table = '$bo_table' and wr_id = '$id' and bf_no = '$i' ");
+
+		    exit;
+		    if ($row[cnt])
+		    {
+		        // 삭제에 체크가 있거나 파일이 있다면 업데이트를 합니다.
+		        // 그렇지 않다면 내용만 업데이트 합니다.
+		        if ($upload[$i][del_check] || $upload[$i][file])
+		        {
+		            $sql = " update $g4[pension_file_table]
+		                        set bf_source = '{$upload[$i][source]}',
+		                            bf_file = '{$upload[$i][file]}',
+		                            bf_content = '{$bf_content[$i]}',
+		                            bf_filesize = '{$upload[$i][filesize]}',
+		                            bf_width = '{$upload[$i][image][0]}',
+		                            bf_height = '{$upload[$i][image][1]}',
+		                            bf_type = '{$upload[$i][image][2]}',
+		                            bf_datetime = '$g4[time_ymdhis]'
+		                      where bo_table = '$bo_table'
+		                        and wr_id = '$id'
+		                        and bf_no = '$i' ";
+		            sql_query($sql);
 		        }
 		        else
-		            $file_script .= "add_file('');\n";
+		        {
+		            $sql = " update $g4[pensoin_file_table]
+		                        set bf_content = '{$bf_content[$i]}'
+		                      where bo_table = '$bo_table'
+		                        and wr_id = '$id'
+		                        and bf_no = '$i' ";
+		            sql_query($sql);
+		        }
 		    }
-		    $file_length = $file[count] - 1;
+		    else
+		    {
+		        $sql = " insert into $g4[pension_file_table]
+		                    set bo_table = '$bo_table',
+		                        wr_id = '$id',
+		                        bf_no = '$i',
+		                        bf_source = '{$upload[$i][source]}',
+		                        bf_file = '{$upload[$i][file]}',
+		                        bf_content = '{$bf_content[$i]}',
+		                        bf_download = 0,
+		                        bf_filesize = '{$upload[$i][filesize]}',
+		                        bf_width = '{$upload[$i][image][0]}',
+		                        bf_height = '{$upload[$i][image][1]}',
+		                        bf_type = '{$upload[$i][image][2]}',
+		                        bf_datetime = '$g4[time_ymdhis]' ";
+		        sql_query($sql);
+		    }
 		}
 
-		if ($file_length < 0)
+		// 업로드된 파일 내용에서 가장 큰 번호를 얻어 거꾸로 확인해 가면서
+		// 파일 정보가 없다면 테이블의 내용을 삭제합니다.
+		$row = sql_fetch(" select max(bf_no) as max_bf_no from $g4[pension_file_table] where bo_table = '$bo_table' and wr_id = '$id' ");
+		for ($i=(int)$row[max_bf_no]; $i>=0; $i--)
 		{
-		    $file_script .= "add_file('');\n";
-		    $file_length = 0;
+		    $row2 = sql_fetch(" select bf_file from $g4[pension_file_table] where bo_table = '$bo_table' and wr_id = '$id' and bf_no = '$i' ");
+
+		    // 정보가 있다면 빠집니다.
+		    if ($row2[bf_file]) break;
+
+		    // 그렇지 않다면 정보를 삭제합니다.
+		    sql_query(" delete from $g4[pension_file_table] where bo_table = '$bo_table' and wr_id = '$id' and bf_no = '$i' ");
 		}
+		//------------------------------------------------------------------------------
+
+
+		Up_Cate($bo_table);
 // File Upload ->
 	} else if($u == "insert") {
 		$r_info_person3 = $r_info_person2 - $r_info_person1;
