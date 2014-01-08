76faa5497f2219a89d19ec69a81fb69beddbc009
Juni7 <linux7@naver.com>
2014-01-08 15:47:02 +0900

    관리자 예약리스트 작업을 위한 링크 및 페이지 생성.

--- skin/board/reser_admin/inc_top_menu.php
+++ skin/board/reser_admin/inc_top_menu.php
@@ -4,7 +4,7 @@ $bo_table2 = "bbs34";
 ?>
 <div class="ui-state-default ui-corner-all" style="margin:5px 0; padding: 5px .7em; text-align:right;">
 	<a href='<?=$g4[bbs_path]?>/board.php?bo_table=<?=$bo_table?>&pension_id=<?=$pension_id?>'>예약홈</a> |
-    <a href='<?=$g4[bbs_path]?>/board.php?bo_table=<?=$bo_table2?>&view_mode=list&pension_id=<?=$pension_id?>'>리스트</a><a href="<?=$g4[bbs_path]?>/board.php?bo_table=<?=$bo_table?>&view_mode=call&pension_id=<?=$pension_id?>">(달력)</a> |
+    <a href='<?=$g4[bbs_path]?>/resList.php?bo_table=<?=$bo_table?>&view_mode=list&pension_id=<?=$pension_id?>'>리스트</a><a href="<?=$g4[bbs_path]?>/board.php?bo_table=<?=$bo_table?>&view_mode=call&pension_id=<?=$pension_id?>">(달력)</a> |
     <a href='<?=$board_skin_path?>/config_room.php?bo_table=<?=$bo_table?>&pension_id=<?=$pension_id?>'>객실/기본요금</a> |
     <a href='<?=$board_skin_path?>/config_date.php?bo_table=<?=$bo_table?>&pension_id=<?=$pension_id?>'>기간별요금</a> |
     <a href='<?=$board_skin_path?>/config_off.php?bo_table=<?=$bo_table?>&pension_id=<?=$pension_id?>'>공휴일</a> |
