102f7e655bb93265fb401b3a0338f31252692b0d
Juni7 <linux7@naver.com>
2014-01-20 09:26:45 +0900

    로그인 페이지.

--- skin/outlogin/staystore/outlogin.skin.1.php
+++ skin/outlogin/staystore/outlogin.skin.1.php
@@ -38,44 +38,52 @@ function chkReset(f)
 
 
 <!-- 로그인 전 외부로그인 시작 -->
-<form name="fhead" method="post" onsubmit="return fhead_submit(this);" autocomplete="off" style="margin:0px;">
+
+<form name="fhead" method="post" onsubmit="return fhead_submit(this);" autocomplete="off" style="margin-left:50%; margin-top:100px;">
 <input type="hidden" name="url" value="<?=$outlogin_url?>">
-<div style="width:220px;">
-    <div style="clear:both;"><img src="<?=$outlogin_skin_path?>/img/login_top.gif" width="220" height="42"></div>
-    <div style="clear:both; float:left; width:5px; height:115px; background:#F8F8F8;"></div>
-    <div style="width:210px; float:left; margin-top:10px;">
-        <table width="210" border="0" cellpadding="0" cellspacing="0">
+<div style="width:500px; margin-left:-250px;">
+    <div style="clear:both;"><img src="<?=$outlogin_skin_path?>/img/login_top.gif" width="500" height="110"></div><!-- 상단로고이미지 -->
+    <div style="clear:both; float:left; width:1px; height:160px; background:#bbbbbb;"></div><!-- 왼쪽세로라인 -->
+
+    <div style="width:498px; float:left; margin-top:40px;"><!-- 아이디/비번  -->
+
+        <table width="498" border="0" cellpadding="0" cellspacing="0">
         <tr>
-            <td width="141">
-                <table width="141" border="0" cellpadding="0" cellspacing="0">
+            <td width="80" border="0" cellpadding="0" cellspacing="0"></td>
+			<td width="248">
+                <table width="248" border="0" cellpadding="0" cellspacing="0">
                 <tr>
-                    <td width="35" height="23"><img src="<?=$outlogin_skin_path?>/img/login_id.gif" width="35" height="23"></td>
-                    <td width="106" height="23" colspan="2" align="center"><input name="mb_id" type="text" class=ed size="12" maxlength="20" required itemname="아이디" value='아이디' onMouseOver='chkReset(this.form);' onFocus='chkReset(this.form);'></td>
+                    <td width="100" height="40"><img src="<?=$outlogin_skin_path?>/img/login_id.gif" width="90" height="30"></td>
+                    <td width="148" height="40" colspan="2"><input name="mb_id" type="text" class=ed size="12" maxlength="20" required itemname="아이디" value='아이디' onMouseOver='chkReset(this.form);' onFocus='chkReset(this.form);'></td>
                 </tr>
                 <tr>
-                    <td width="35" height="23"><img src="<?=$outlogin_skin_path?>/img/login_pw.gif" width="35" height="23"></td>
-                    <td id=pw1 width="106" height="23" colspan="2" align="center"><input type="text" class=ed size="12" maxlength="20" required itemname="패스워드" value='패스워드' onMouseOver='chkReset(this.form);' onfocus='chkReset(this.form);'></td>
-                    <td id=pw2 style='display:none;' width="106" height="23" colspan="2" align="center"><input name="mb_password" id="outlogin_mb_password" type="password" class=ed size="12" maxlength="20" itemname="패스워드" onMouseOver='chkReset(this.form);' onfocus='chkReset(this.form);' onKeyPress="check_capslock(event, 'outlogin_mb_password');"></td>
+                    <td width="100" height="30"><img src="<?=$outlogin_skin_path?>/img/login_pw.gif" width="90" height="30"></td>
+                    <td id=pw1 width="148" height="30" colspan="2"><input type="text" class=ed size="12" maxlength="20" required itemname="패스워드" value='패스워드' onMouseOver='chkReset(this.form);' onfocus='chkReset(this.form);'></td>
+                    <td id=pw2 style='display:none;' width="148" height="30" colspan="2" align="center"><input name="mb_password" id="outlogin_mb_password" type="password" class=ed size="12" maxlength="20" itemname="패스워드" onMouseOver='chkReset(this.form);' onfocus='chkReset(this.form);' onKeyPress="check_capslock(event, 'outlogin_mb_password');"></td>
                 </tr>
                 </table>
             </td>
-            <td width="69" height="46" rowspan="2" align="center"><input type="image" src="<?=$outlogin_skin_path?>/img/login_button.gif" width="52" height="46"></td>
+            <td width="170" height="" rowspan="2" align="center"><input type="image" src="<?=$outlogin_skin_path?>/img/login_btn.gif" width="90" height="70"></td><!-- 오른쪽로그인버튼 -->
         </tr>
         </table>
-        <div style="clear:both; padding:2px 0 0 42px;">
+
+        <div style="clear:both; padding:10px 0 0 100px;"><!-- 자동로그인 -->
             <div style="float:left;"><input type="checkbox" name="auto_login" value="1" onclick="if (this.checked) { if (confirm('자동로그인을 사용하시면 다음부터 회원아이디와 패스워드를 입력하실 필요가 없습니다.\n\n\공공장소에서는 개인정보가 유출될 수 있으니 사용을 자제하여 주십시오.\n\n자동로그인을 사용하시겠습니까?')) { this.checked = true; } else { this.checked = false; } }"></div>
-            <div style="float:left; padding-left:5px;"><img src="<?=$outlogin_skin_path?>/img/login_auto.gif" width="46" height="28"></div>
-        </div>
-        <div style="clear:both; padding:0 0 0 42px;">
-            <!-- <a href="javascript:win_password_forget();"><img src="<?=$outlogin_skin_path?>/img/login_pw_find_button.gif" width="90" height="20" border="0"></a> -->
-            <a href="javascript:win_password_lost();"><img src="<?=$outlogin_skin_path?>/img/login_pw_find_button.gif" width="90" height="20" border="0"></a>
-            <a href="<?=$g4[bbs_path]?>/register.php"><img src="<?=$outlogin_skin_path?>/img/login_join_button.gif" width="69" height="20" border="0"></a>
-        </div>
+            <div style="float:left; padding-left:5px;">자동로그인</div>
+        </div> 		
     </div>
-    <div style="float:left; width:5px; height:115px; background:#F8F8F8;"></div>
-    <div style="clear:both;"><img src="<?=$outlogin_skin_path?>/img/login_down.gif" width="220" height="14"></div>
+
+    <div style="float:right; width:1px; height:160px; background:#bbbbbb;"></div><!-- 오른쪽세로라인 -->
+    <div style="clear:both; width:500px; height:1px; background:#bbbbbb;"><!-- <img src="<?=$outlogin_skin_path?>/img/login_down.gif" width="220" height="14"> --></div>
+	<div style="clear:both; padding:20px 0 0 100px;">
+            <!-- <a href="javascript:win_password_forget();"><img src="<?=$outlogin_skin_path?>/img/login_pw_find_button.gif" width="90" height="20" border="0"></a> -->
+            <a href="javascript:win_password_lost();"><img src="<?=$outlogin_skin_path?>/img/login_pw_find_button.gif" width="150" height="30" border="0"></a>
+            <a href="<?=$g4[bbs_path]?>/register.php"><img src="<?=$outlogin_skin_path?>/img/login_join_button.gif" width="150" height="30" border="0"></a>
+        </div>
 </div>
 </form>
+
+
 
 <script type="text/javascript">
 function fhead_submit(f)
--- skin/outlogin/staystore/outlogin.skin.2.php
+++ skin/outlogin/staystore/outlogin.skin.2.php
@@ -2,57 +2,58 @@
 ?>
 
 <!-- 로그인 후 외부로그인 시작 -->
-<table width="220" border="0" cellspacing="0" cellpadding="0">
-<tr>
-    <td width="220" colspan="5"><img src="<?=$outlogin_skin_path?>/img/login_ing_top.gif" width="220" height="42"></td>
-</tr>
-<tr>
-    <td width="5" rowspan="4" background="<?=$outlogin_skin_path?>/img/login_left_bg.gif"></td>
-    <td width="210" colspan="3"></td>
-    <td width="5" rowspan="4" background="<?=$outlogin_skin_path?>/img/login_right_bg.gif"></td>
-</tr>
-<tr>
-    <td colspan="3">
-        <table width="210" height="27" border="0" cellpadding="0" cellspacing="0">
-        <tr>
-            <td width="25" height="27"><img src="<?=$outlogin_skin_path?>/img/login_ing_icon.gif" width="25" height="27"></td>
-            <td width="139" height="27"><span class='member'><strong><?=$nick?></strong></span>님</td>
-            <td width="46" height="27"><?php if ($is_admin == "super" || $is_auth) { ?><a href="<?=$g4['admin_path']?>/"><img src="<?=$outlogin_skin_path?>/img/admin.gif" width="33" height="15" border="0" align="absmiddle"></a><?php } ?></td>
-        </tr>
-      </table></td>
-</tr>
-<tr>
-    <td width="25"></td>
-    <td width="160" height="25" align="center" bgcolor="#F9F9F9"><a href="javascript:win_point();"><font color="#737373">포인트 : <?=$point?>점</font></a></td>
-    <td width="25"></td>
-</tr>
-<tr>
-    <td colspan="3">
-        <table width="210" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-            <td>
-                <table width="210" height="50" border="0" cellpadding="0" cellspacing="0">
-                <tr>
-                    <td width="25"></td>
-                    <td width="82"><a href="<?=$g4['bbs_path']?>/logout.php"><img src="<?=$outlogin_skin_path?>/img/logout_button.gif" width="78" height="20" border="0"></a></td>
-                    <td width="78"><a href="<?=$g4['bbs_path']?>/member_confirm.php?url=register_form.php"><img src="<?=$outlogin_skin_path?>/img/login_modify.gif" width="78" height="20" border="0"></a></td>
-                    <td width="25"></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td align="center"><a href="javascript:win_memo();"><FONT color="#ff8871;"><B>쪽지 (<?=$memo_not_read?>)</B></FONT></a></td>
-                    <td><a href="javascript:win_scrap();"><img src="<?=$outlogin_skin_path?>/img/scrap_button.gif" width="78" height="20" border="0"></a></td>
-                    <td></td>
-                </tr>
-                </table></td>
-        </tr>
-        </table></td>
-</tr>
-<tr>
-    <td colspan="5"><img src="<?=$outlogin_skin_path?>/img/login_down.gif" width="220" height="14"></td>
-</tr>
-</table>
-
+<div style="margin-left:50%; margin-top:100px;">
+	<div style="width:500px; border:1px solid #bbb; margin-left:-250px;">
+	<table width="500" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td width="500" colspan="5"><img src="<?=$outlogin_skin_path?>/img/login_ing_top.gif" width="500" height="110"></td>
+	</tr>
+	<tr height="40"></tr>
+	<tr>
+		<td width="50" rowspan="4"></td>
+		<td width="400" colspan="3"></td>
+		<td width="50" rowspan="4"></td>
+	</tr>
+	<tr>
+		<td colspan="3" align="center">
+			<table width="400" height="40" border="0" cellpadding="0" cellspacing="0" align="center">
+			<tr>
+				<td width="30" height="30"><img src="<?=$outlogin_skin_path?>/img/login_ing_icon.gif" width="25" height="27"></td>
+				<td width="220" height="30"><span class='member'><strong><?=$nick?></strong></span>님</td>
+				<td width="150" height="30"><?php if ($is_admin == "super" || $is_auth) { ?><a href="<?=$g4['admin_path']?>/"><img src="<?=$outlogin_skin_path?>/img/admin.gif" width="150" height="30" border="0" align="absmiddle"></a><?php } ?></td>
+			</tr>
+		  </table></td>
+	</tr>
+	<tr height="40"></tr>
+	<!-- <tr>
+		<td width="25"></td>
+		<td width="160" height="25" align="center" bgcolor="#F9F9F9"><a href="javascript:win_point();"><font color="#737373">포인트 : <?=$point?>점</font></a></td>
+		<td width="25"></td>
+	</tr> --><!-- 포인트 -->
+	<tr>
+		<td colspan="3">
+			<table width="400" height="40" border="0" cellspacing="0" cellpadding="0">
+			<tr>
+				<td>
+					<table width="400" border="0" cellpadding="0" cellspacing="0" >
+						<tr height="50" >
+							<td width="175"><a href="<?=$g4['bbs_path']?>/logout.php"><img src="<?=$outlogin_skin_path?>/img/logout_button.gif" width="150" height="30" border="0"></a></td>
+							<td width="175"><a href="<?=$g4['bbs_path']?>/member_confirm.php?url=register_form.php"><img src="<?=$outlogin_skin_path?>/img/login_modify.gif" width="150" height="30" border="0"></a></td>
+						</tr>
+						<tr height="50">
+							<td align="center"><a href="javascript:win_memo();"><FONT color="#ff8871;"><B>쪽지 (<?=$memo_not_read?>)</B></FONT></a></td>
+							<td><a href="javascript:win_scrap();"><img src="<?=$outlogin_skin_path?>/img/scrap_button.gif" width="150" height="30" border="0"></a></td>
+						</tr>
+					</table></td>
+			</tr>
+			</table></td>
+	</tr>
+	<tr>
+		<td height="20" colspan="5"></td>
+	</tr>
+	</table>
+	</div>
+</div>
 <script type="text/javascript">
 // 탈퇴의 경우 아래 코드를 연동하시면 됩니다.
 function member_leave()
