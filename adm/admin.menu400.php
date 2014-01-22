b693e86053f64a3eeef27d610b5149c47ba5af49
Juni7 <linux7@naver.com>
2014-01-22 09:54:14 +0900

    pension_id 가 있을 경우 메뉴 변경

--- adm/admin.menu400.php
+++ adm/admin.menu400.php
@@ -1,12 +1,21 @@
 <?php
-$menu["menu400"] = array (
-    array("400000", "펜션관리", ""),
-    array("400100", "펜션등록", "$g4[admin_path]/pen_admin.php?mode=reg"),
-    array("-"),
-	array("400300", "예약관리", "$g4[admin_path]/pen_admin.php?mode=reserhome&pension_id={$pension_id}"),
-	array("-"),
-	array("400310", "입금현황", "$g4[admin_path]/admin.stats.php?pension_id={$pension_id}")
-);
+if($pension_id) {
+	$menu["menu400"] = array (
+	    array("400000", "펜션관리", ""),
+	    array("400100", "펜션등록", "$g4[admin_path]/pen_admin.php?mode=reg"),
+	    array("-"),
+		array("400300", "예약관리", "$g4[admin_path]/pen_admin.php?mode=reserhome&pension_id={$pension_id}"),
+		array("-"),
+		array("400310", "입금현황", "$g4[admin_path]/admin.stats.php?pension_id={$pension_id}")
+	);
+} else {
+	$menu["menu400"] = array (
+	    array("400000", "펜션관리", ""),
+	    array("400100", "펜션등록", "$g4[admin_path]/pen_admin.php?mode=reg"),
+	    array("-"),
+		array("400310", "입금현황", "$g4[admin_path]/admin.stats.php")
+	);
+}
 ?>
 
 
