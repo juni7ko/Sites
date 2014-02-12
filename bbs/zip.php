7dde16af3b18c1ccbe38160597651febaad3a34c
Juni7 <linux7@naver.com>
2014-02-12 09:58:08 +0900

    도로명주소

--- bbs/zip.php
+++ bbs/zip.php
@@ -1,9 +1,97 @@
-<?php include_once("./_common.php");
+<?php
+include_once("./_common.php");
+
+// 검색 타입에 따른 검색단어 설정
+if($type == "old") { $addr1 = $addr4; }
+if($type == "newdong") { $addr1 = $addr2." ".$addr3; }
+
+if ($addr1)
+{
+
+	// 신주소 소켓통신 요청
+	function HTTP_Post($URL,$data) {
+		$URL_Info=parse_url($URL);
+		if(!empty($data)) foreach($data AS $k => $v) $str .= urlencode($k).'='.urlencode($v).'&';
+		$path = $URL_Info["path"];
+		$host = $URL_Info["host"];
+		$port = $URL_Info["port"];
+		if (empty($port)) $port=80;
+
+		$result = "";
+		$fp = fsockopen($host, $port, $errno, $errstr, 30);
+		$http  = "POST $path HTTP/1.0\r\n";
+		$http .= "Host: $host\r\n";
+		$http .= "Content-Type: application/x-www-form-urlencoded\r\n";
+		$http .= "Content-length: " . strlen($str) . "\r\n";
+		$http .= "Connection: close\r\n\r\n";
+		$http .= $str . "\r\n\r\n";
+		fwrite($fp, $http);
+		while (!feof($fp)) { $result .= fgets($fp, 4096); }
+		fclose($fp);
+		return $result;
+	}
+
+	$url  = "http://post.phpschool.com/phps.kr"; // 신주소 api URL
+	$data = array("addr"=>$addr1, "ipkey"=>"2051114", "charset"=>"UTF-8", "type"=>$type); // UTF-8일경우 "UTF-8" 로 기재
+
+	$output = (HTTP_Post($url, $data));
+	$output = substr($output, strpos($output,"\r\n\r\n")+4);
+
+	$output = unserialize($output);
+	$result = $output['result'];
+
+	if ($result > 0) {
+		$post_data = unserialize($output['post']);
+
+		for ($i=0; $i<$result; $i++) {
+
+			//$post_data[$i]['post'];              // 우편번호
+			//$post_data[$i]['addr_1'];            // 시/도
+			//$post_data[$i]['addr_2'];            // 구
+			//$post_data[$i]['addr_3'];            // 도로명
+			//$post_data[$i]['addr_4'];            // 동/건물
+			$list[$i][zip1] = substr($post_data[$i]['post'],0,3);
+			$list[$i][zip2] = substr($post_data[$i]['post'],3,3);
+			$list[$i][addr] = $post_data[$i]['addr_1']." ".$post_data[$i]['addr_2']." ".$post_data[$i]['addr_3'];
+			$list[$i][addr2] = $post_data[$i]['addr_4'];
+			$list[$i][bunji] = "<br /><span style='padding-left:1px;'>[ 구주소 : ".$post_data[$i]['addr_5']." ]</span>";
+			$list[$i][encode_addr] = urlencode($list[$i][addr]);
+		}
+	} else if ($result == 0) {
+
+		alert("찾으시는 주소가 없습니다.");
+
+	} else if ($result < 0) {
+
+		switch($result) {
+			case "-1":
+				alert("검색결과가 너무 많습니다. 2단어 이상 조합해 주세요.");
+				break;
+			case "-2":
+				alert("미인증 IP입니다. http://post.phpschool.com/join.html에 접속하여 API를 신청해 주세요.");
+				break;
+			case "-3":
+				alert("1일 조회 횟수가 초과되었습니다.");
+				break;
+			case "-4":
+				alert("이메일 인증을 완료하지 않은 API 사용자입니다.");
+				break;
+		}
+
+		// $result  "-1"  일경우 :  너무많은검색결과 1000건이상
+		// $result  "-2"  일경우 :  서버 IP 미인증
+		// $result  "-3"  일경우 :  조회횟수초과
+		// $result  "-4"  일경우 :  미인증 사용자
+	}
+
+	$search_count = $output[result];
+}
+
 
 // 메모리를 많이 잡아먹어서 아래의 코드로 대체
 //ini_set('memory_limit', '20M');
 //$zipfile = file("./zip.db");
-
+/*
 $zipfile = array();
 $fp = fopen("./zip.db", "r");
 while(!feof($fp)) {
@@ -13,17 +101,17 @@ fclose($fp);
 
 $search_count = 0;
 
-if ($addr1) 
+if ($addr1)
 {
-    while ($zipcode = each($zipfile)) 
+    while ($zipcode = each($zipfile))
     {
         if(strstr(substr($zipcode[1],9,512), $addr1))
         {
             $list[$search_count][zip1] = substr($zipcode[1],0,3);
-            $list[$search_count][zip2] = substr($zipcode[1],4,3);    
+            $list[$search_count][zip2] = substr($zipcode[1],4,3);
             $addr = explode(" ", substr($zipcode[1],8));
 
-            if ($addr[sizeof($addr)-1]) 
+            if ($addr[sizeof($addr)-1])
             {
                 $list[$search_count][addr] = str_replace($addr[sizeof($addr)-1], "", substr($zipcode[1],8));
                 $list[$search_count][bunji] = trim($addr[sizeof($addr)-1]);
@@ -33,20 +121,20 @@ if ($addr1)
 
             $list[$search_count][encode_addr] = urlencode($list[$search_count][addr]);
             $search_count++;
-        }    
+        }
     }
 
     if (!$search_count) alert("찾으시는 주소가 없습니다.");
 }
 
 /* 기존의 DB에서 불러오는 방식
-if ($addr1) 
+if ($addr1)
 {
     //$sql = " select * from $g4[zip_table] where zp_dong like '%$addr1%' order by zp_id ";
     $sql = " select * from $g4[zip_table] where zp_dong like '%$addr1%' order by zp_sido, zp_gugun, zp_dong ";
     $result = sql_query($sql);
     $search_count = 0;
-    for ($i=0; $row=sql_fetch_array($result); $i++) 
+    for ($i=0; $row=sql_fetch_array($result); $i++)
     {
         $list[$i][zip1] = substr($row[zp_code], 0, 3);
         $list[$i][zip2] = substr($row[zp_code], 3, 3);
@@ -56,7 +144,7 @@ if ($addr1)
         $search_count++;
     }
 
-    if (!$search_count) 
+    if (!$search_count)
         alert("찾으시는 주소가 없습니다.");
 }
 */
--- skin/member/basic/zip.skin.php
+++ skin/member/basic/zip.skin.php
@@ -1,4 +1,5 @@
-<?php if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가 
+<?php
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 
 <table width="600" border="0" cellspacing="0" cellpadding="0">
@@ -8,13 +9,13 @@
 <input type=hidden name=frm_zip2  value='<?=$frm_zip2?>'>
 <input type=hidden name=frm_addr1 value='<?=$frm_addr1?>'>
 <input type=hidden name=frm_addr2 value='<?=$frm_addr2?>'>
-<tr> 
+<tr>
     <td colspan="2">
         <table width="100%" height="50" border="0" cellpadding="0" cellspacing="0">
-        <tr> 
+        <tr>
             <td align="center" valign="middle" bgcolor="#EBEBEB">
                 <table width="98%" height="40" border="0" cellspacing="0" cellpadding="0">
-                <tr> 
+                <tr>
                     <td width="5%" align="center" bgcolor="#FFFFFF" ><img src="<?=$g4[bbs_img_path]?>/icon_01.gif" width="5" height="5"></td>
                     <td width="35%" align="left" bgcolor="#FFFFFF" ><font color="#666666"><b><?=$g4[title]?></b></font></td>
                     <td width="60%" bgcolor="#FFFFFF" ></td>
@@ -23,17 +24,53 @@
         </tr>
         </table></td>
 </tr>
-<tr> 
-    <td height="70" colspan="2" valign="bottom"><img src="<?=$g4[bbs_img_path]?>/zip_img_01.gif" width="273" height="40"></td>
-</tr>
-<tr> 
+<tr>
     <td height="20" colspan="2"></td>
 </tr>
-<tr> 
-    <td width=130><img src="<?=$g4[bbs_img_path]?>/zip_img_02.gif" width="125" height="14"></td>
-    <td><input type=text name=addr1 value='<?=$addr1?>' required minlength=2 itemname='동(읍/면/리)' size=35> <input type=image src='<?=$g4[bbs_img_path]?>/btn_post_search.gif' border=0 align=absmiddle></td>
+<tr>
+    <td width=130 style="padding-left:60px;"><strong>검색 타입</strong></td>
+    <td>
+		<input type="radio" onclick="addr_change('new');" checked value="new" name="type">도로/건물명 통합검색
+		<input type="radio" onclick="addr_change('newdong');" <?=($type=="newdong")?"checked":"";?> value="newdong" name="type">구주소(동,번지)
+		<input type="radio" onclick="addr_change('old');" <?=($type=="old")?"checked":"";?> value="old" name="type">구주소(동,읍/면/리)
+	</td>
 </tr>
-<tr> 
+<tr>
+    <td height="50" colspan="2" valign="bottom" style="padding-left:60px;">
+		<div class="addrs new" <?=($type=="new" || $type=="")?"":"style='display:none;'";?>>
+			※ 찾고자 하시는 도로명 주소나 건물명을 한단어 또는 여러 단어로 입력하세요.</br>
+			<span style="padding-left:15px;">예) 가산동 에이스하이엔드, 덕양구 행신동 햇빛마을, 한글비석로24</span>
+		</div>
+		<div class="addrs newdong" <?=($type=="newdong")?"":"style='display:none;'";?>>
+			※ 찾고자 하시는 구 주소지를 동과 번지로 구분하여 입력하세요.
+			</br><span style="padding-left:15px;">예) 대치동 907, 상계5동 155-48, 가산동 371-50</span>
+		</div>
+		<div class="addrs old" <?=($type=="old")?"":"style='display:none;'";?>>
+			※ 찾고자 하시는 구 주소의 동(읍/면/리) 이름을 입력하세요.</br>
+			<span style="padding-left:15px;">예) 가산동, 수유4동, 상계45동</span>
+		</div>
+	</td>
+</tr>
+<tr>
+    <td height="20" colspan="2"></td>
+</tr>
+<tr>
+    <td width=130 style="padding-left:60px;"><strong>검색 주소</strong></td>
+    <td>
+		<div class="addrs new" <?=($type=="new" || $type=="")?"":"style='display:none;'";?>>
+			<input type=text name=addr1 value='<?=$addr1?>' size=35> <input type=image src='<?=$g4[bbs_img_path]?>/btn_post_search.gif' border=0 align=absmiddle>
+		</div>
+		<div class="addrs newdong" <?=($type=="newdong")?"":"style='display:none;'";?>>
+			<input type=text name=addr2 value='<?=($addr2)?$addr2:"동";?>' onclick="if(this.value=='동'){this.value='';}"; size=10>
+			<input type=text name=addr3 value='<?=($addr3)?$addr3:"번지";?>' onclick="if(this.value=='번지'){this.value='';}"; size=10>
+			<input type=image src='<?=$g4[bbs_img_path]?>/btn_post_search.gif' border=0 align=absmiddle>
+		</div>
+		<div class="addrs old" <?=($type=="old")?"":"style='display:none;'";?>>
+			<input type=text name=addr4 value='<?=$addr4?>' size=35> <input type=image src='<?=$g4[bbs_img_path]?>/btn_post_search.gif' border=0 align=absmiddle>
+		</div>
+	</td>
+</tr>
+<tr>
     <td height="20" colspan="2"></td>
 </tr>
 </table>
@@ -41,27 +78,35 @@
 
 <script type='text/javascript'>
 document.fzip.addr1.focus();
+
+function addr_change(addr_type) {
+	$(".addrs").hide();
+	$("."+addr_type).show();
+}
 </script>
 
 
 <?php if ($search_count > 0) { ?>
 <table width="600" border="0" cellspacing="0" cellpadding="0">
-<tr> 
+<tr>
     <td height="1" colspan="2" background="<?=$g4[bbs_img_path]?>/post_dot_bg.gif"></td>
 </tr>
-<tr> 
+<tr>
     <td height="50" colspan="2"><img src="<?=$g4[bbs_img_path]?>/zip_img_03.gif" width="99" height="13"></td>
 </tr>
-<tr> 
+<tr>
     <td width="10%"></td>
     <td width="90%">
         <table width=100% cellpadding=0 cellspacing=0>
         <tr>
-            <td height=23 valign=top>총 <?=$search_count?>건 가나다순</td>
+            <td height=23 valign=top>총 <?=$search_count?>건 가나다순 (검색시간 : <?=$output[time];?>초)</td>
         </tr>
-        <?php for ($i=0; $i<count($list); $i++) 
+        <?php
+        for ($i=0; $i<count($list); $i++)
         {
-            echo "<tr><td height=19><a href='javascript:;' onclick=\"find_zip('{$list[$i][zip1]}', '{$list[$i][zip2]}', '{$list[$i][addr]}');\">{$list[$i][zip1]}-{$list[$i][zip2]} : {$list[$i][addr]} {$list[$i][bunji]}</a></td></tr>\n";
+            echo "<tr><td height=19><a href='javascript:;' onclick=\"find_zip('{$list[$i][zip1]}', '{$list[$i][zip2]}', '{$list[$i][addr]}');\">{$list[$i][zip1]}-{$list[$i][zip2]} : {$list[$i][addr]} {$list[$i][addr2]}";
+			if($type != "old") echo $list[$i][bunji];
+			echo "</a></td></tr>\n";
         }
         ?>
         <tr>
@@ -84,6 +129,11 @@ function find_zip(zip1, zip2, addr1)
     of.<?=$frm_addr2?>.focus();
     window.close();
     return false;
+}
+
+function addr_change(addr_type) {
+	$(".addrs").hide();
+	$("."+addr_type).show();
 }
 </script>
 <?php } ?>
